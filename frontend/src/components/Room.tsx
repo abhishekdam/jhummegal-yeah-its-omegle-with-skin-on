c090c737c51c06bbf9ae802754e93f370c5bd85b
Abhishek Dam <damforwork247@gmail.com>
2026-05-11 13:26:18 +0530

    starting backend building starting from socket io

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -5,7 +5,9 @@ const Room = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const name = searchParams.get("name");
 
-  useEffect(() => {}, [name]);
+  useEffect(() => {
+    // login for join room
+  }, [name]);
 
   return <>Hi {name} !</>;
 };
